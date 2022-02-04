5763dd97b9c9767c5cae5740e39f5394269bf075
vberger <vberger@users.noreply.github.com>
2022-02-04 09:21:09 +0000

    deploy: 2e14f954b081bfb12dd404bcce1fddb7caff5abf

--- api/calloop/ping/sidebar-items.js
+++ api/calloop/ping/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["make_ping","Create a new ping event source"]],"struct":[["Ping","The Ping handle"],["PingSource","The ping event source"]]});
+initSidebarItems({"fn":[["make_ping","Create a new ping event source"]],"struct":[["Ping","The Ping handle"],["PingError","An error arising from processing events for a ping."],["PingSource","The ping event source"]]});
--- api/calloop/timer/sidebar-items.js
+++ api/calloop/timer/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["Timeout","An itentifier to cancel a timeout if necessary"],["Timer","A Timer event source"],["TimerHandle","An handle to a timer, used to set or cancel timeouts"]]});
+initSidebarItems({"struct":[["Timeout","An itentifier to cancel a timeout if necessary"],["Timer","A Timer event source"],["TimerError","An error arising from processing events for a timer."],["TimerHandle","An handle to a timer, used to set or cancel timeouts"]]});
