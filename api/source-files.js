78e37c394eec607e4b2b934b370b2a5da664001d
vberger <vberger@users.noreply.github.com>
2022-03-06 14:19:46 +0000

    deploy: f07649334f2d5bb60c61fed6a29621857da4a9ab

--- api/source-files.js
+++ api/source-files.js
@@ -1,3 +1,3 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["calloop"] = {"name":"","dirs":[{"name":"sources","dirs":[{"name":"timer","files":["mod.rs","timerfd.rs"]}],"files":["channel.rs","futures.rs","generic.rs","mod.rs","ping.rs","signals.rs"]},{"name":"sys","files":["epoll.rs","mod.rs"]}],"files":["error.rs","io.rs","lib.rs","loop_logic.rs"]};
+sourcesIndex["calloop"] = {"name":"","dirs":[{"name":"sources","dirs":[{"name":"timer","files":["mod.rs","timerfd.rs"]}],"files":["channel.rs","futures.rs","generic.rs","mod.rs","ping.rs","signals.rs"]},{"name":"sys","files":["epoll.rs","mod.rs"]}],"files":["error.rs","io.rs","lib.rs","loop_logic.rs","macros.rs"]};
 createSourceSidebar();
